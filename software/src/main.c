81b4c412ce20cf5269e7b519df0739d8ae9253aa
Ben Campbell <bjc68@cam.ac.uk>
2023-10-31 16:55:48 +0000

    Add functions for testing on FPGA

--- software/src/main.c
+++ software/src/main.c
@@ -1,7 +1,58 @@
 #include "bubbleSort.h"
+#include "debug.h"
 
-int x=20;
 
+#define FRAMEBUFFER_BASE 0x08000000
+// our pixel format in memory is 5 bits of red, 6 bits of green, 5 bits of blue
+#define PIXEL16(r,g,b) (((r & 0x1F)<<11) | ((g & 0x3F)<<5) | ((b & 0x1F)<<0))
+// ... but for ease of programming we refer to colours in 8/8/8 format and discard the lower bits
+#define PIXEL24(r,g,b) PIXEL16((r>>3), (g>>2), (b>>3))
+
+#define PIXEL_WHITE PIXEL24(0xFF, 0xFF, 0xFF)
+#define PIXEL_BLACK PIXEL24(0x00, 0x00, 0x00)
+#define PIXEL_RED   PIXEL24(0xFF, 0x00, 0x00)
+#define PIXEL_GREEN PIXEL24(0x00, 0xFF, 0x00)
+#define PIXEL_BLUE  PIXEL24(0x00, 0x00, 0xFF)
+
+#define DISPLAY_WIDTH	480
+#define DISPLAY_HEIGHT	272
+
+volatile int x=20;
+int y=21;
+int z=22;
+
+
+void vid_set_pixel(int x, int y, int colour)
+{
+    // derive a pointer to the framebuffer described as 16 bit integers
+    volatile short *framebuffer = (volatile short *) (FRAMEBUFFER_BASE);
+
+    // make sure we don't go past the edge of the screen
+    if ((x<0) || (x>DISPLAY_WIDTH-1))
+        return;
+    if ((y<0) || (y>DISPLAY_HEIGHT-1))
+        return;
+
+    framebuffer[x+y*DISPLAY_WIDTH] = colour;
+}
+
+void vid_set_bg(int colour){
+    for (int x = 0; x < DISPLAY_WIDTH; x++){
+        for (int y = 0; y < DISPLAY_HEIGHT; y++){
+            vid_set_pixel(x, y, colour);
+        }
+    }
+}
+
+void hex_output(unsigned long value){
+    volatile unsigned long *hex_leds = (unsigned long *) 0x04000080;
+    *hex_leds = value;
+}
+
+void set_screen_mem(unsigned long value, unsigned long offset){
+    volatile unsigned long *screen = (unsigned long *) 0x08000000;
+    screen[offset] = value;
+}
 int mult(int a, int b){
     int result = 0;
     int c=b;
@@ -13,5 +64,32 @@ int mult(int a, int b){
 
 int main(void) {
     x = mult(x, x);
+    dprint_str("Hello world");
     bubbleSortBenchMark();
+    unsigned long counter = 0x0;
+    char r = 255;
+    char g = 0;
+    char b = 0;
+    char phase = 0;
+    while (1){
+        if (phase == 0){
+            if (++g == 255) phase = 1;
+        } else if (phase == 1) {
+            if (--r == 0) phase = 2;
+        } else if (phase == 2) {
+            if (++b == 255) phase = 3;
+        } else if (phase == 3) {
+            if (--g == 0) phase = 4;
+        } else if (phase == 4) {
+            if (++r == 255) phase = 5;
+        } else if (phase == 5) {
+            if (--b == 0) phase = 0;
+        }
+        
+        vid_set_bg(PIXEL24(r,g,b));
+        for (int i = 0; i < 50000; i++){
+            hex_output(i);
+            x=mult(i,i);
+        }
+    }
 }
